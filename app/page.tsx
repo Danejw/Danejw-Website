368f0cbb6afbbe2f40483b7163ac02683318e05d
Danejw <48073085+Danejw@users.noreply.github.com>
2025-12-14 18:39:01 -1000

    Prevent horizontal overflow on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { TemplateLandingPage } from "@/app/components/TemplateLandingPage";
 
 export default function Home() {
   return (
-    <main className="font-[family-name:var(--font-geist-sans)]">
+    <main className="font-[family-name:var(--font-geist-sans)] overflow-x-hidden">
       <TemplateLandingPage />
     </main>
   );
